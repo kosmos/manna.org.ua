933a45886206f2eb4ce8cd685c188e39608042f4
Kosmos <kosmos.ehp@gmail.com>
2017-12-23 16:35:52 +0200

    Moves firebase init to separate file

---
+++ src/api/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAWfEb_U8tVs5ukZe9_hwdvIIe_6Av-d8c',
+  authDomain: 'manna-deb1d.firebaseapp.com',
+  databaseURL: 'https://manna-deb1d.firebaseio.com',
+  projectId: 'manna-deb1d',
+  storageBucket: 'manna-deb1d.appspot.com',
+  messagingSenderId: '208419616390',
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import firebase from 'firebase';
+
+import firebase from './api/firebase';
 import App from './App';
 import router from './router';
 
@@ -9,15 +10,7 @@ import router from './router';
 Vue.config.productionTip = false;
 
 let app;
-const config = {
-  apiKey: 'AIzaSyAWfEb_U8tVs5ukZe9_hwdvIIe_6Av-d8c',
-  authDomain: 'manna-deb1d.firebaseapp.com',
-  databaseURL: 'https://manna-deb1d.firebaseio.com',
-  projectId: 'manna-deb1d',
-  storageBucket: 'manna-deb1d.appspot.com',
-  messagingSenderId: '208419616390',
-};
-firebase.initializeApp(config);
+
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
     /* eslint-disable no-new */
